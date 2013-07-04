341812ff3b689a0652072c7add245895f3f17e57
wonyoung <wonyoungjjang@gmail.com>
2013-07-04 11:24:37 +0900

    c2/13 encode returns adding "1" when word's size is over 1

--- src/Soundex.java
+++ src/Soundex.java
@@ -2,7 +2,11 @@
 class Soundex {
 
 	public String encode(String word) {
-		return zeroPad(word);
+		String encoded = word.substring(0, 1);
+		if (word.length() > 1) {
+			encoded += "1";
+		}
+		return zeroPad(encoded);
 	}
 
 	private String zeroPad(String word) {
