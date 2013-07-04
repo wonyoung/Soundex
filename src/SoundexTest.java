28896af976c993f0c180bc3921039dd68ce2e9e5
wonyoung <wonyoungjjang@gmail.com>
2013-07-04 10:48:28 +0900

    first commit

---
+++ src/SoundexTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SoundexTest {
+
+	@Test
+	public void retainsSoleLetterOfOneLetterWord() {
+		Soundex soundex;
+	}
+
+}
